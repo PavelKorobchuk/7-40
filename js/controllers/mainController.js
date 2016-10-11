19bd77ea94a602b77a58966e60e29ff4057bd0de
PavelKorobchuk <pavelkorobchuk@gmail.com>
2016-10-11 20:57:47 +0300

    Add files via upload

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,1 +1,41 @@
+'use strict';
+myApp.controller('mainController', ['$scope', '$location', function ($scope, $location) {
+    $scope.signUp = signUp;
+    $scope.signIn = signIn;
+    $scope.userSuccess = false;
+    $scope.userError = false;
+    function signUp(userName, userPass) {
+        if (userName && userPass !== '') {
+            var newUser = {
+                name: userName,
+                pass: userPass,
+            };
+            var noteItem = JSON.stringify(newUser);
+            localStorage.setItem(userName, noteItem);
+            $scope.userSuccess = true;
+            $scope.userError = false;
+        } else {
+            $scope.userError = true;
+        }
 
+    }
+
+    function signIn(userName, userPass) {
+        if (localStorage.getItem(userName) === null) {
+            $scope.userError = true;
+        } else {
+            $scope.userError = false;
+            var returnUserName = JSON.parse(localStorage.getItem(userName));
+            if (returnUserName.pass == userPass) {
+                $location.path('/passList');
+
+            }else{
+                $scope.userError = true;
+                $scope.userSuccess = false;
+            }
+
+        }
+
+
+    }
+}]);
--- js/controllers/passListController.js
+++ js/controllers/passListController.js
@@ -1,1 +1,65 @@
+'use strict';
+myApp.controller('passListController', ['$scope', function ($scope) {
 
+    $scope.passList = passList;
+    $scope.savePass = savePass;
+    $scope.addRow = addRow;
+    $scope.removeItem = removeItem;
+    $scope.typePass = typePass;
+    $scope.itemArray = [];
+    $scope.inputType = 'password';
+    var newItem = {};
+    $scope.passEdit = {
+        editable: false,
+        setEditableTrue: function () {
+            this.editable = true;
+        },
+        setEditableFalse: function () {
+            this.editable = false;
+        }
+    };
+    function addRow(linkName, loginName, passName) {
+        var userName = loginName;
+        var userPass = passName;
+        var userLink = linkName;
+        savePass(userName, userPass, userLink);
+        $scope.itemArray.unshift(newItem);
+    }
+
+    function passList() {
+        if (localStorage.length > 0) {
+            for (var i = 0; i < localStorage.length; i++) {
+                var parseStorage = JSON.parse(localStorage.getItem(localStorage.key(i)));
+                if (parseStorage.id == 1) {
+                    $scope.itemArray.push(parseStorage)
+                }
+            }
+        }
+        console.log($scope.itemArray)
+    }
+
+    passList();
+
+
+    function savePass(userName, userPass, userLink) {
+        newItem = {
+            id: 1,
+            name: userName,
+            pass: userPass,
+            link: userLink
+        };
+        var noteItem = JSON.stringify(newItem);
+        localStorage.setItem(userName, noteItem);
+        $scope.passEdit.setEditableFalse();
+    }
+
+    function removeItem(index) {
+        var curItem = $scope.itemArray[index];
+        $scope.itemArray.splice(index, 1);
+        localStorage.removeItem(curItem.name)
+    }
+
+    function typePass(index) {
+        $scope.itemArray[index].inputType = 'text';
+    }
+}]);
